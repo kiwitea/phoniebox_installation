ec45fc11abcb8f446b5b6e80919fd7485c4b25ce
kiwitea <emptyal@gmail.com>
2024-03-16 22:07:58 +0100

    Update mopidy-restart-to-login.sh

--- usr/bin/mopidy-restart-to-login.sh
+++ usr/bin/mopidy-restart-to-login.sh
@@ -6,6 +6,10 @@ SERVICE_NAME="mopidy"
 if systemctl is-active --quiet "$SERVICE_NAME"; then
     echo "Service $SERVICE_NAME is running. Restarting..."
     systemctl restart "$SERVICE_NAME"
+
+    # play startup sound, with default volume 20 if not set
+    mpgvolume=$((32768*${AUDIOVOLBOOT:-20}/100))
+    /usr/bin/mpg123 -f -${mpgvolume} /home/pi/RPi-Jukebox-RFID/shared/short-success-sound-glockenspiel.mp3
 else
     echo "Service $SERVICE_NAME is not running."
 fi
